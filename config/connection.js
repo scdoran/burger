9f268f2f2d57f3a8501644ca3895d47c76486391
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2017-04-05 22:48:54 -0400

    Trying out JAWSDB.

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
+  host: "y2w3wxldca8enczv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "vdmwi7l5eqghx2qw",
+  password: "bn2q6765y9bfia20",
   database: "burgers_db"
 });
 
